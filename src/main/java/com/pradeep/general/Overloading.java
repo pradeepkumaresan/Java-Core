ce2a4726e86f3b155d9f26f5fce8d560a79c98f6
pradeepkumaresan <pradeep.it39@gmail.com>
2019-09-18 15:44:17 +0530

    dummy commit

---
+++ src/main/java/com/pradeep/general/Overloading.java
@@ -0,0 +1,23 @@
+package com.pradeep.general;
+
+import java.util.List;
+
+public class Overloading {
+    public void print(String s){
+
+
+    }
+
+    public void print(Object s){
+
+    }
+
+    public void print(List<String> s){
+
+    }
+
+    /*public void print(List<Object> s){
+
+    }*/
+
+}
